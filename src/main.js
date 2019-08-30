7a4fa1667b52ce20bd7326ae714ad50892ff2094
Alberto Masia <alberto.masia@gmail.com>
2019-08-30 13:12:48 +0200

    Remove former app content
    add bootstrap and other libraries to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/style.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+export const eventBus = new Vue();
+
 Vue.config.productionTip = false
 
 new Vue({
