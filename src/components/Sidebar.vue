c08ae7cf24a1d3226f4174694a46f52e48a7ff46
Alberto Masia <alberto.masia@gmail.com>
2019-08-30 13:31:59 +0200

    Display and filter the different types of messages

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,28 +6,28 @@
       <li :class="{active: activeView === 'app-inbox'}">
         <a href="#" @click.prevent="navigate('app-inbox', 'Inbox')">
           <i class="fa fa-inbox"></i>Inbox
-          <span class="label label-danger pull-right">?</span>
+          <span class="label label-danger pull-right">{{ unreadMessages.length }}</span>
         </a>
       </li>
 
       <li :class="{active: activeView === 'app-sent'}">
         <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
           <i class="fa fa-envelope-o"></i>Sent
-          <span class="label label-default pull-right">?</span>
+          <span class="label label-default pull-right">{{ sentMessages.length }}</span>
         </a>
       </li>
 
       <li :class="{active: activeView === 'app-important'}">
         <a href="#" @click.prevent="navigate('app-important', 'Important')">
           <i class="fa fa-bookmark-o"></i>Important
-          <span class="label label-warning pull-right">?</span>
+          <span class="label label-warning pull-right">{{ importantMessages.length }}</span>
         </a>
       </li>
 
       <li :class="{active: activeView === 'app-trash'}">
         <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
           <i class="fa fa-trash-o"></i>Trash
-          <span class="label label-default pull-right">?</span>
+          <span class="label label-default pull-right">{{ trashedMessages.length }}</span>
         </a>
       </li>
     </ul>
@@ -39,6 +39,12 @@ import { eventBus } from "../main";
 import User from './User';
 
 export default {
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    }
+  },
   components: {
     appUser: User
   },
@@ -55,6 +61,28 @@ export default {
         title
       });
     }
+  },
+  computed: {
+    unreadMessages() {
+      return this.messages.filter(message => {
+        return (message.type === 'incoming' && !message.isRead && !message.isDeleted);
+      })
+    },
+    sentMessages() {
+      return this.messages.filter(message => {
+        return (message.type === 'outgoing' && !message.isDeleted);
+      })
+    },
+    importantMessages() {
+      return this.messages.filter(message => {
+        return (message.type === 'incoming' && message.isImportant && !message.isDeleted);
+      })
+    },
+    trashedMessages() {
+      return this.messages.filter(message => {
+        return message.isDeleted;
+      })
+    },
   }
 };
 </script>
